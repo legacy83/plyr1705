631c4c5ea8b0b981be04500607923211f058235d
Thiago Senna <trsenna@gmail.com>
2017-05-27 20:58:08 -0300

    initial modules for api and admin

---
+++ modules/api/v01/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\api\v01;
+
+/**
+ * api-v01 module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\api\v01\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
