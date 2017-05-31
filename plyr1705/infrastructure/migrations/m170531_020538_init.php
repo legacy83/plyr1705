781ed8640ee85a4da1aa304088b39af19357e92d
Thiago Senna <trsenna@gmail.com>
2017-05-30 23:35:48 -0300

    basic infrastructure migration added

--- plyr1705/infrastructure/migrations/m170531_020538_init.php
+++ plyr1705/infrastructure/migrations/m170531_020538_init.php
@@ -10,13 +10,31 @@ class m170531_020538_init extends Migration
 {
     public function up()
     {
+        $tableOptions = null;
+        if ( $this->db->driverName === 'mysql' ) {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
 
+        $this->createTable( '{{%project}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique(),
+            'title' => $this->string()->notNull(),
+            'content' => $this->text()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions );
+
+        $this->createTable( '{{%project_meta}}', [
+            'id' => $this->primaryKey(),
+            'project_id' => $this->integer()->notNull(),
+            'key' => $this->string()->notNull(),
+            'value' => $this->string(),
+        ], $tableOptions );
     }
 
     public function down()
     {
-        echo "m170531_020538_init cannot be reverted.\n";
-
-        return false;
+        $this->dropTable( '{{%project}}' );
+        $this->dropTable( '{{%project_meta}}' );
     }
 }
