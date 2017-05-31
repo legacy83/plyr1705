adf676608e84312d1ee23429e40425d4f0b29094
vagrant <vagrant@plyr1705>
2017-05-30 23:47:33 -0300

    more complete migration done

--- plyr1705/infrastructure/migrations/m170531_020538_init.php
+++ plyr1705/infrastructure/migrations/m170531_020538_init.php
@@ -15,7 +15,7 @@ class m170531_020538_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable( '{{%project}}', [
+        $this->createTable( 'project', [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull()->unique(),
             'title' => $this->string()->notNull(),
@@ -24,17 +24,22 @@ class m170531_020538_init extends Migration
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions );
 
-        $this->createTable( '{{%project_meta}}', [
+        $this->createTable( 'project_meta', [
             'id' => $this->primaryKey(),
             'project_id' => $this->integer()->notNull(),
             'key' => $this->string()->notNull(),
             'value' => $this->string(),
         ], $tableOptions );
+
+        $this->createIndex( 'idx-project_meta-project_id', 'project_meta', 'project_id' );
+        $this->addForeignKey( 'fk-project_meta-project_id', 'project_meta', 'project_id', 'project', 'id' );
     }
 
     public function down()
     {
-        $this->dropTable( '{{%project}}' );
-        $this->dropTable( '{{%project_meta}}' );
+        $this->dropForeignKey( 'fk-project_meta-project_id', 'project_meta' );
+        $this->dropIndex( 'idx-project_meta-project_id', 'project_meta' );
+        $this->dropTable( 'project_meta' );
+        $this->dropTable( 'project' );
     }
 }
