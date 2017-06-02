16c6e3f77896e8d5bfbba0bb0613f7ff95937473
Thiago Senna <trsenna@gmail.com>
2017-06-02 01:59:35 -0300

    simplified active record

--- plyr1705/domain/model/project/ProjectRecord.php
+++ plyr1705/domain/model/project/ProjectRecord.php
@@ -8,28 +8,34 @@ use yii\db\ActiveRecord;
 /**
  * Class ProjectRecord
  *
- * @property integer           $id
- * @property string            $name
- * @property string            $title
- * @property string            $content
- * @property integer           $created_at
- * @property integer           $updated_at
- * @property ProjectMetaRecord $client
+ * @property integer $id
+ * @property string  $name
+ * @property string  $title
+ * @property string  $content
+ * @property integer $created_at
+ * @property integer $updated_at
  *
  * @package plyr1705\domain\model\project
  */
 class ProjectRecord extends ActiveRecord
 {
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return array
+     */
     public function behaviors()
     {
         return [
             TimestampBehavior::className(),
-            ClientBehavior::className(),
         ];
-    }
-
-    public static function tableName()
-    {
-        return '{{%project}}';
     }
 }
--- plyr1705/infrastructure/migrations/m170531_020538_init.php
+++ plyr1705/infrastructure/migrations/m170531_020538_init.php
@@ -10,36 +10,27 @@ class m170531_020538_init extends Migration
 {
     public function up()
     {
-        $tableOptions = null;
-        if ( $this->db->driverName === 'mysql' ) {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
-
         $this->createTable( 'project', [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull()->unique(),
             'title' => $this->string()->notNull(),
             'content' => $this->text()->notNull(),
+            'client_name' => $this->string()->notNull(),
+            'thumbnail_src' => $this->string()->notNull(),
+            'thumbnail_alt' => $this->string(),
+            'featured_src' => $this->string()->notNull(),
+            'featured_alt' => $this->string(),
+            'work1_src' => $this->string()->notNull(),
+            'work1_alt' => $this->string(),
+            'work2_src' => $this->string()->notNull(),
+            'work2_alt' => $this->string(),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
-        ], $tableOptions );
-
-        $this->createTable( 'project_meta', [
-            'id' => $this->primaryKey(),
-            'project_id' => $this->integer()->notNull(),
-            'key' => $this->string()->notNull(),
-            'value' => $this->string(),
-        ], $tableOptions );
-
-        $this->createIndex( 'idx-project_meta-project_id', 'project_meta', 'project_id' );
-        $this->addForeignKey( 'fk-project_meta-project_id', 'project_meta', 'project_id', 'project', 'id' );
+        ] );
     }
 
     public function down()
     {
-        $this->dropForeignKey( 'fk-project_meta-project_id', 'project_meta' );
-        $this->dropIndex( 'idx-project_meta-project_id', 'project_meta' );
-        $this->dropTable( 'project_meta' );
         $this->dropTable( 'project' );
     }
 }
