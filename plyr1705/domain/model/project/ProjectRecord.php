e059d28fe5c49fad4fabd8356a95b142bf9f0322
Thiago Senna <trsenna@gmail.com>
2017-06-03 22:59:29 -0300

    saving progress

--- plyr1705/domain/model/project/ProjectRecord.php
+++ plyr1705/domain/model/project/ProjectRecord.php
@@ -8,14 +8,14 @@ use yii\db\ActiveRecord;
 /**
  * Class ProjectRecord
  *
- * @property integer $id
- * @property string  $title
- * @property string  $content
- * @property integer $created_at
- * @property integer $updated_at
- * @property integer $client_id
- * @property integer $thumbnail_id
- * @property integer $featured_id
+ * @property integer          $id
+ * @property string           $title
+ * @property string           $content
+ * @property integer          $created_at
+ * @property integer          $updated_at
+ * @property ClientRecord     $client
+ * @property MediaImageRecord $thumbnail
+ * @property MediaImageRecord $featured
  *
  * @package plyr1705\domain\model\project
  */
@@ -37,5 +37,22 @@ class ProjectRecord extends ActiveRecord
         return [
             TimestampBehavior::className(),
         ];
+    }
+
+    public function getClient()
+    {
+        return $this->hasOne( ClientRecord::className(), [ 'id' => 'client_id' ] );
+    }
+
+    public function getThumbnail()
+    {
+        $query = $this->hasOne( MediaImageRecord::className(), [ 'id' => 'thumbnail_id' ] );
+        return $query->where( '`key` = :key', [ ':key' => 'thumbnail' ] );
+    }
+
+    public function getFeatured()
+    {
+        $query = $this->hasOne( MediaImageRecord::className(), [ 'id' => 'featured_id' ] );
+        return $query->where( '`key` = :key', [ ':key' => 'featured' ] );
     }
 }
